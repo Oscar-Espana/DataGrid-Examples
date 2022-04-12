42884f59a38300caaa1b172d8083862aac66711b
Oscar España <espaaoscar@gmail.com>
2022-04-11 21:32:33 -0500

    datagrid to show agencyConsignees data

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,106 @@
-import React from "react";
+import type { NextPage } from "next";
+import DataGrid, {
+  Editing,
+  Column,
+  Selection,
+  Summary,
+  GroupItem,
+  GroupPanel,
+  SearchPanel,
+  SortByGroupSummaryInfo,
+} from "devextreme-react/data-grid";
+import { agencyConsignees } from "../src/constants/agencyConsignees";
+import { useState } from "react";
 
-const index = () => {
-  return <div>index</div>;
+const columns = ["CompanyName", "City", "State", "Phone", "Fax"];
+
+const Home: NextPage = () => {
+  const [data, setData] = useState(agencyConsignees);
+
+  const onRowUpdated = (e) => {
+    setData([...data].map((i) => (i.id === e.key ? { ...e.data } : i)));
+  };
+
+  return (
+    <div>
+      <h1>Hola Mundo</h1>
+      <DataGrid
+        id="gridContainerAC"
+        keyExpr="id"
+        dataSource={data}
+        showBorders={true}
+        onRowUpdated={onRowUpdated}
+      >
+        <Selection mode="single" />
+        <Editing
+          mode="batch"
+          allowUpdating={true}
+          allowAdding={false}
+          allowDeleting={false}
+          selectTextOnEditStart={true}
+          startEditAction="dblClick"
+        />
+        <GroupPanel visible={true} />
+        <SearchPanel visible={true} placeholder="Buscar " />
+        <Column
+          dataField="agencyName"
+          width={130}
+          caption="Agencia"
+          allowEditing={false}
+        />
+        <Column
+          dataField="consigneeName"
+          width={160}
+          caption="Consignatario"
+          allowEditing={false}
+        />
+        <Column dataField="edit.valueKg" width={160} caption="Editable" />
+        <Column dataField="close.valueKg" width={160} caption="Cierre" />
+        <Column
+          dataField="structural.valueKg"
+          width={160}
+          caption="Estructura"
+        />
+        <Column dataField="booking.valueKg" width={160} caption="Reserva" />
+        <Column dataField="agencyName" caption="Agencia" groupIndex={1} />
+        <Column dataField="day" caption="Dia" groupIndex={0} />
+
+        <Summary>
+          <GroupItem
+            column="agencyName"
+            summaryType="count"
+            displayFormat="{0} registros"
+          />
+          <GroupItem
+            column="edit.valueKg"
+            alignByColumn={true}
+            summaryType="sum"
+            displayFormat="Total: {0}"
+          />
+          <GroupItem
+            column="close.valueKg"
+            alignByColumn={true}
+            summaryType="sum"
+            displayFormat="Total: {0}"
+          />
+
+          <GroupItem
+            column="structural.valueKg"
+            alignByColumn={true}
+            summaryType="sum"
+            displayFormat="Total: {0}"
+          />
+          <GroupItem
+            column="booking.valueKg"
+            alignByColumn={true}
+            summaryType="sum"
+            displayFormat="Total: {0}"
+          />
+        </Summary>
+        <SortByGroupSummaryInfo summaryItem="count" />
+      </DataGrid>
+    </div>
+  );
 };
 
-export default index;
+export default Home;
---
+++ src/constants/agencyConsignees.ts
@@ -0,0 +1,179 @@
+export const agencyConsignees = [
+  {
+    id: 1,
+    day: "LUNES",
+    product: "FRUTA",
+    agencyName: "NAVECUADOR",
+    agencyId: "24b12203-5b19-4a55-ba54-230f9bc4e7ab",
+    consigneeName: "Consignatario 1",
+    consigneeId: "2c3c3895-4f17-43d8-9c4e-51adb2cc08ee",
+    edit: {
+      tag: "",
+      valueKg: 10,
+      valueMt: 0,
+    },
+    close: {
+      tag: "",
+      valueKg: 1.4,
+      valueMt: 0,
+    },
+    booking: {
+      tag: "",
+      valueKg: 7.6,
+      valueMt: 0,
+    },
+    structural: {
+      tag: "",
+      valueKg: 80,
+      valueMt: 0,
+    },
+  },
+  {
+    id: 2,
+    day: "LUNES",
+    product: "FRUTA",
+    agencyName: "NAVECUADOR",
+    agencyId: "24b12203-5b19-4a55-ba54-230f9bc4e7ab",
+    consigneeName: "Consignatario 2",
+    consigneeId: "1d4d4906-4f17-43d8-9c4e-51adb2cc08ee",
+    edit: {
+      tag: "",
+      valueKg: 50,
+      valueMt: 0,
+    },
+    close: {
+      tag: "",
+      valueKg: 60,
+      valueMt: 0,
+    },
+    booking: {
+      tag: "",
+      valueKg: 30,
+      valueMt: 0,
+    },
+
+    structural: {
+      tag: "",
+      valueKg: 20,
+      valueMt: 0,
+    },
+  },
+  {
+    id: 3,
+    day: "LUNES",
+    product: "FRUTA 2",
+    agencyName: "LATAM",
+    agencyId: "53G12509-5b19-4a55-ba54-230f9bc4e7ab",
+    consigneeName: "Consignatario 2",
+    consigneeId: "1d4d4906-4f17-43d8-9c4e-51adb2cc08ee",
+    edit: {
+      tag: "",
+      valueKg: 6.99,
+      valueMt: 0,
+    },
+    close: {
+      tag: "",
+      valueKg: 1.99,
+      valueMt: 0,
+    },
+    booking: {
+      tag: "",
+      valueKg: 7,
+      valueMt: 0,
+    },
+
+    structural: {
+      tag: "",
+      valueKg: 10,
+      valueMt: 0,
+    },
+  },
+  {
+    id: 4,
+    day: "MARTES",
+    product: "FRUTA",
+    agencyName: "LATAM",
+    agencyId: "53G12509-5b19-4a55-ba54-230f9bc4e7ab",
+    consigneeName: "Consignatario 2",
+    consigneeId: "1d4d4906-4f17-43d8-9c4e-51adb2cc08ee",
+    edit: {
+      tag: "",
+      valueKg: 6.99,
+      valueMt: 0,
+    },
+    close: {
+      tag: "",
+      valueKg: 1.99,
+      valueMt: 0,
+    },
+    booking: {
+      tag: "",
+      valueKg: 7,
+      valueMt: 0,
+    },
+    structural: {
+      tag: "",
+      valueKg: 10,
+      valueMt: 0,
+    },
+  },
+  {
+    id: 5,
+    day: "MARTES",
+    product: "FRUTA",
+    agencyName: "LATAM",
+    agencyId: "53G12509-5b19-4a55-ba54-230f9bc4e7ab",
+    consigneeName: "Consignatario 1",
+    consigneeId: "2c3c3895-4f17-43d8-9c4e-51adb2cc08ee",
+    edit: {
+      tag: "",
+      valueKg: 6.99,
+      valueMt: 0,
+    },
+    close: {
+      tag: "",
+      valueKg: 1.99,
+      valueMt: 0,
+    },
+    booking: {
+      tag: "",
+      valueKg: 7,
+      valueMt: 0,
+    },
+
+    structural: {
+      tag: "",
+      valueKg: 10,
+      valueMt: 0,
+    },
+  },
+  {
+    id: 6,
+    day: "MARTES",
+    product: "FRUTA 2",
+    agencyName: "NAVECUADOR",
+    agencyId: "24b12203-5b19-4a55-ba54-230f9bc4e7ab",
+    consigneeName: "Consignatario 1",
+    consigneeId: "2c3c3895-4f17-43d8-9c4e-51adb2cc08ee",
+    edit: {
+      tag: "",
+      valueKg: 10,
+      valueMt: 0,
+    },
+    close: {
+      tag: "",
+      valueKg: 1.4,
+      valueMt: 0,
+    },
+    booking: {
+      tag: "",
+      valueKg: 7.6,
+      valueMt: 0,
+    },
+    structural: {
+      tag: "",
+      valueKg: 80,
+      valueMt: 0,
+    },
+  },
+];
